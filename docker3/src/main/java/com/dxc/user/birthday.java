539ee895a133956df50229abf21a6ad0e98ee7b2
ebhuvan <ebhuvan@IN-PG02LYA8>
2021-11-24 09:55:34 +0530

    birthday class added

---
+++ docker3/src/main/java/com/dxc/user/birthday.java
@@ -0,0 +1,18 @@
+package com.dxc.user;
+
+public class birthday {
+public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	public int getName() {
+		return name;
+	}
+	public void setName(int name) {
+		this.name = name;
+	}
+private int roll;
+private int name;
+}
